e83df1a346a6d866d99048e596566d10034b330e
Raman Krytski <rkeydevelop@gmail.com>
2023-12-20 15:35:48 +0100

    refactoring: code clean up

--- 1/1.js
+++ 1/1.js
@@ -1,21 +1,6 @@
-/* 
-1) С помощью встроенной браузерной функции prompt поочерёдно ввести два значения. 
-Если оба значения являются валидными числами, то вывести в консоли результат в виде 
-первого числа в системе счисления второго.
-Если хотя бы одно из введённых значений является некорректным числом, 
-вывести в консоли сообщение: "Некорректный ввод!" и завершить программу.
-> Примеры:
-Вводим 10 и 2, получаем 1010
-Вводим 872 и 8, получаем 1550
-Вводим 2 и 'abc', получаем "Некорректный ввод!"
-*/
-function toNumber(value) {
-  return +value;
-}
-
 function convertNumber(num1, num2) {
-  const num = toNumber(num1);
-  const radix = toNumber(num2);
+  const num = +num1;
+  const radix = +num2;
   const isSafeNums = Number.isSafeInteger(num) && Number.isSafeInteger(radix);
   const isSafeRadix = radix > 1 && radix < 37;
 
@@ -23,7 +8,7 @@ function convertNumber(num1, num2) {
     return console.log('Некорректный ввод!');
   }
 
-  return console.log(toNumber(num.toString(radix)) || num.toString(radix));
+  return console.log(+num.toString(radix) || num.toString(radix));
 }
 
 convertNumber(prompt(), prompt());
