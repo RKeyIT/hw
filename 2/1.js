ce3f97d35e48a5eb71881e1b62f1ad5327ea1307
Raman Krytski <rkeydevelop@gmail.com>
2023-12-20 16:26:23 +0100

    2: 1/3 done

---
+++ 2/1.js
@@ -0,0 +1,21 @@
+function getType(el) {
+  return Object.prototype.toString
+    .call(el)
+    .replace(/.\w*.([A-Z]\w*)./, '$1')
+    .toLowerCase();
+}
+
+function makeObjectDeepCopy(object) {
+  if (typeof object !== 'object') {
+    return object;
+  }
+  if (getType(object) === 'object') {
+    for (const key in object) {
+      makeObjectDeepCopy({ ...object[key] });
+    }
+  }
+  if (getType(object) === 'array') {
+    return [...object].map(makeObjectDeepCopy);
+  }
+  return object;
+}
