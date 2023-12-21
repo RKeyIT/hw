fa5aa898fc72c22e8cfb9de06b186e5a98c08d7a
Raman Krytski <rkeydevelop@gmail.com>
2023-12-21 16:45:37 +0100

    4: 2/2 are done!

--- 4-Pending/2.js
+++ 4-Pending/2.js
@@ -1,8 +1,10 @@
 /* 
 2) Создать класс Calculator. Конструктор класса должен принимать два валидных числа, 
 иначе (если параметра не два или хотя бы один из них невалидный number) бросать 
-ошибку (throw new Error('')). Данный класс должен иметь 
-методы setX, setY, logSum, logMul, logSub, logDiv.
+ошибку (throw new Error('')). 
+
+Данный класс должен иметь методы 
+setX, setY, logSum, logMul, logSub, logDiv.
 - setX(num) - задаёт первому из чисел новое значение. 
     Кидать ошибку если параметр не передан или является невалидным числом;
 - setY(num) - задаёт второму из чисел новое значение. 
@@ -33,4 +35,54 @@ P.s. Infinity, -Infinity и NaN - это невалидные числа (дел
 Задание новое, так что там есть ошибки и нужно будет ещё дополнять условия
 */
 
-class Calculator {}
+class Calculator {
+  constructor(x, y) {
+    if (!x || !y) {
+      throw new Error('Одно или два числа невалидны!');
+    }
+
+    Calculator.x = x;
+    Calculator.y = y;
+  }
+
+  setX(x) {
+    if (typeof x === 'number' && !isNaN(x) && isFinite(x)) {
+      return (Calculator.x = x);
+    }
+
+    throw new Error('Переданное число невалидно!');
+  }
+
+  setY(y) {
+    if (typeof y === 'number' && !isNaN(y) && isFinite(y)) {
+      return (Calculator.y = y);
+    }
+
+    throw new Error('Переданное число невалидно!');
+  }
+
+  logSum() {
+    console.log(Calculator.x + Calculator.y);
+  }
+  logMul() {
+    console.log(Calculator.x * Calculator.y);
+  }
+  logSub() {
+    console.log(Calculator.x - Calculator.y);
+  }
+  logDiv() {
+    if (Calculator.y === 0) {
+      throw new Error('На ноль делить нельзя!');
+    }
+    console.log(Calculator.x / Calculator.y);
+  }
+}
+
+const calculator = new Calculator(12, 3);
+calculator.logSum(); // 15
+calculator.logDiv(); // 4
+calculator.setX(15);
+calculator.logDiv(); // 5
+const logCalculatorDiv = calculator.logDiv;
+logCalculatorDiv(); // 5
+calculator.setY(444n); // Ошибка!
