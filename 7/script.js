7cc32dbcbe3e6f9e0e0f069c011bad69d99a0d46
Raman Krytski <rkeydevelop@gmail.com>
2023-12-24 18:54:46 +0100

    feat: first version of view is implemented

--- 7/script.js
+++ 7/script.js
@@ -39,3 +39,64 @@ class Calculator {
 }
 
 const calculator = new Calculator();
+
+const UIDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(createButton);
+const UIOps = ['+', '-', '*', '/', '+/-', '->', 'C', '.', '='].map(
+  createButton
+);
+
+function createButton(el) {
+  const btn = document.createElement('button');
+  const text = document.createTextNode(el);
+  btn.appendChild(text);
+  btn.className = `btn ${el}`;
+  btn.type = 'button';
+
+  if (typeof el === 'number') {
+    btn.style = `grid-area: d${el}`;
+  } else {
+    btn.style = `grid-area: ${applyGridAreaToSign(el)}`;
+  }
+
+  return btn;
+}
+
+function applyGridAreaToSign(el) {
+  switch (el) {
+    case '+':
+      return 'plus';
+    case '-':
+      return 'minus';
+    case '*':
+      return 'multiply';
+    case '/':
+      return 'divide';
+    case '+/-':
+      return 'invert';
+    case '->':
+      return 'delete';
+    case 'C':
+      return 'reset';
+    case '.':
+      return 'dot';
+    case '=':
+      return 'equal';
+    default:
+      throw new Error('Somebody brokes the apllication!');
+  }
+}
+
+console.log(UIDigits);
+console.log(UIOps);
+
+const btnUI = document.getElementById('buttons');
+
+function renderButtons() {
+  for (let i = 0; i < UIDigits.length; i++) {
+    btnUI.appendChild(UIDigits[i]);
+  }
+  for (let i = 0; i < UIOps.length; i++) {
+    btnUI.appendChild(UIOps[i]);
+  }
+}
+renderButtons();
